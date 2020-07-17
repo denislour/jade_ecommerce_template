3d8497bf825fd6a4380c9991ae80a4ee527c1652
Lektor <denislour@gmail.com>
2020-07-17 13:00:33 -0400

    [Templates] - Add cart page.

--- app.py
+++ app.py
@@ -45,5 +45,11 @@ def article():
     return render_template('pages/article.jade')
 
 
+@app.route('/cart')
+def cart():
+    return render_template('pages/cart.jade')
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
